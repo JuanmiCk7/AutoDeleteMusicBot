6c5371128009211329c805a1f295b3e02372a5dc
JuanmiCk7 <73592738+JuanmiCk7@users.noreply.github.com>
2021-12-28 17:11:48 +0100

    hello

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const config = require('./config.json')
 //const words = ['!play', '=play', '-play']
 
