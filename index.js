b084c1e9d0d04faa682b179e9e3f135e270d6bbe
JuanmiCk7 <73592738+JuanmiCk7@users.noreply.github.com>
2021-12-29 00:24:13 +0100

    Adding skippy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { createAudioPlayer } = require('@discordjs/voice');
+
 
 const words = ['!play', '=play', '-play']
 
