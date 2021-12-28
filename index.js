4cb667df1be02976a52d096c0214f9f23f7a9b1b
JuanmiCk7 <73592738+JuanmiCk7@users.noreply.github.com>
2021-12-28 16:18:32 +0100

    adding Procfile and modified index.js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
+const words = ['!play', '=play', '-play']
 
 client.on('ready', () => {
     console.log('The client is ready!')
+
+})
+
+client.on('message', message => {
+    var content = message.content;
+
+    for (var i = 0; i < words.length; i++) {
+        if (content.includes(words[i])) {
+            message.delete()
+            break
+        }
+    }
+
+    if (message.author.id === '185476724627210241') {
+        message.delete()
+    }
 })
 
 client.login(config.token)
