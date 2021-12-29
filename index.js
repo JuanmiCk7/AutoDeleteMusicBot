3e5210ad3daac407da2e67a333008f9dcac712d6
JuanmiCk7 <73592738+JuanmiCk7@users.noreply.github.com>
2021-12-29 02:05:04 +0100

    Fix

--- index.js
+++ index.js
@@ -10,55 +10,56 @@ client.on('ready', () => {
 })
 
 client.on('message', message => {
-    if (message.member.permissions.has('SEND_MESSAGES') && message.member.permissions.has('CONNECT') && message.member.permissions.has('SPEAK')) {
+    if (message.member.permissions.has('SEND_MESSAGES') && message.member.permissions.has('CONNECT') && client.hasPermission('SPEAK')) {
 
         var content = message.content;
-
-    for (var i = 0; i < words.length; i++) {
-        if (content.includes(words[i])) {
-            console.log('Delete message!')
-            message.delete()
-            break
+        if (message.member.permissions.has('MANAGE_MESSAGES')) {
+            for (var i = 0; i < words.length; i++) {
+                if (content.includes(words[i])) {
+                    console.log('Delete message!')
+                    message.delete()
+                    break
+                }
+            }
         }
+        if (message.member.permissions.has('MANAGE_MESSAGES')) {
+            if (message.author.id === '185476724627210241') {
+                console.log("Message from Ayana!")
+                setTimeout(() => message.delete(), 5000)
+
+            }
+        }
+        if (content === "!illo") {
+            message.channel.send('Illo que pafaa');
+        }
+
+        if (message.content.startsWith('!skippy')) {
+
+            if (!message.member.voice.channel) return message.reply("Tienes q estar en un canal brode!");
+            // Checking if the bot is in a voice channel.
+            if (message.guild.me.voice.channel) return message.reply("Illo, que ya estoy aqui!");
+
+            // Joining the channel and creating a VoiceConnection.
+            message.member.voice.channel.join().then(VoiceConnection => {
+
+                // Playing the music, and, on finish, disconnecting the bot.
+                VoiceConnection.play("./resources/skippy.mp3").on("finish", () => VoiceConnection.disconnect());
+                message.reply("Ha llegado Skippy!");
+            }).catch(e => console.log(e))
+        }
+
+
+    } else {
+        console.log('The bot dont have permission!')
     }
-
-    if (message.author.id === '185476724627210241') {
-        console.log("Message from Ayana!")
-        setTimeout(() => message.delete(), 5000)
-        
-    }
-
-    if (content === "!illo") {
-        message.channel.send('Illo que pafaa');
-    }
-
-    if(message.content.startsWith('!skippy')) {
-
-        if (!message.member.voice.channel) return message.reply("Tienes q estar en un canal brode!");
-        // Checking if the bot is in a voice channel.
-        if (message.guild.me.voice.channel) return message.reply("Illo, que ya estoy aqui!");
-
-        // Joining the channel and creating a VoiceConnection.
-        message.member.voice.channel.join().then(VoiceConnection => {
-
-        // Playing the music, and, on finish, disconnecting the bot.
-        VoiceConnection.play("./resources/skippy.mp3").on("finish", () => VoiceConnection.disconnect());
-            message.reply("Ha llegado Skippy!");
-        }).catch(e => console.log(e))
-    }
-
-  
-  } else {
-      console.log('The bot dont have permission!')
-  }
 
 })
 
 function sleep(ms) {
     return new Promise((resolve) => {
-      setTimeout(resolve, ms);
+        setTimeout(resolve, ms);
     });
-  }
+}
 
 client.login(process.env.TOKEN)
 
